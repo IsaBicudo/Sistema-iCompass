7074d580128b1ac708e7251a4e48603e3124f56c
LirySouza <lirianysouza15@gmail.com>
2024-06-21 09:09:02 -0300

    Tela Login Adicionada

--- Program.cs
+++ Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Usuario}/{action=Login}");
 
 app.Run();
